5b5ed590e988557c1350c8d4fd74311f49dc9faa
Irina Herzberg <rirarum@yandex.com>
2020-10-12 22:51:11 +0200

    Cards are added with JS

--- scripts/script.js
+++ scripts/script.js
@@ -28,9 +28,10 @@ const cards = [
 const places = document.querySelector('.places');
 const addButton = document.querySelector('.profile__add-button');
 const template = document.querySelector('.template');
-const inputPlaceName = document.querySelector('')
+const inputPlaceName = document.querySelector('.popup__input-field_value_place');
 
 const popupOpened = document.querySelector('.profile__edit-button');
+const popupCards = document.querySelector('.profile__add-button');
 const popupClosed = document.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__form');
 const popup = document.querySelector('.popup');
@@ -40,6 +41,16 @@ const nameInput = document.querySelector('.popup__input-field_value_name');
 const jobInput = document.querySelector('.popup__input-field_value_job');
 
 
+const getCards = (data) => {
+  return (`<div class="place">
+            <div class="place__cover" style="background-image:url(${data.backgroundImage});"></div>
+              <div class="place__description">
+              <h2 class="place__title">${data.title}</h2>
+              <button type="button" class="place__like" aria-label="Добавить в избранное"></button>
+             </div>
+          </div>`);
+};
+
 const renderCards = () => {
   const items = cards.map(element => {
     return getCards(element);
@@ -48,37 +59,27 @@ const renderCards = () => {
   places.insertAdjacentHTML('afterbegin', items);
 };
 
-const getCards = (data) => {
-  return `<div class="place">
-            <div class="place__cover" style="background-image:url(${element.backgroundImage});"></div>
-              <div class="place__description">
-              <h2 class="place__title">${element.title}</h2>
-              <button type="button" class="place__like" aria-label="Добавить в избранное"></button>
-             </div>
-          </div>`;
-};
+renderCards();
+
 
 const addCards = () => {
   addButton.addEventListener('click', () => {
     const item = getCards( {
-      title: ''
+      title: inputPlaceName.value
     });
-  places.insertAdjacentHTML('afterbegin', item);
+   places.insertAdjacentHTML('afterbegin', item);
   })
 };
 
 
-renderCards();
-
-/*
-function openPopup () {r
+function openPopup () {
   nameInput.value = userName.textContent;
   jobInput.value = job.textContent;
-  popup.classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
 function closePopup () {
-  popup.classList.toggle('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -90,8 +91,11 @@ function formSubmitHandler (evt) {
   closePopup ();
 }
 
+
+popupCards.addEventListener('click', openPopup);
+
 popupOpened.addEventListener('click', openPopup);
 
 popupClosed.addEventListener('click', closePopup);
 
-formElement.addEventListener('submit', formSubmitHandler); */
+formElement.addEventListener('submit', formSubmitHandler);
