fb82935f4f691b40ba92c1df1a17ff484fc85900
Irina Herzberg <66871126+space-boss@users.noreply.github.com>
2020-12-14 21:32:46 +0100

    Revert "Refactor8/popupwithform"

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,50 +1,39 @@
-import {Card} from '../components/Card.js';
+import {Card} from '../components/card.js';
 import {FormValidator} from '../utils/formValidator.js';
-import {Section} from '../components/Section.js';
-
-import {Popup} from '../components/Popup.js';
-import {PopupWithImage} from '../components/PopupWithImg.js';
-import {PopupWithForm} from '../components/PopupWithForm.js';
-import {UserInfo} from '..//components/UserInfo.js';
-
+import {Section} from '../components/section.js';
 import {
-  cards,
-  places,
-  addPlace,
-  placeForm,
-  template,
-  inputPlaceName,
-  inputPlaceUrl,
-  editProfile,
-
-  formElement,
-  popupProfileSelector,
-  popupPlaceSelector,
-  popupZoomSelector,
-
-  userName,
-  job,
-  zoomPlaceImg,
-  zoomPlaceCaption
+  cards
 } from '../utils/constants.js';
+import {Popup} from '../components/popup.js';
+import {PopupWithImage} from '../components/popupimg.js'
 
 
-const popupPlace = new PopupWithForm({
-  popupSelector: popupPlaceSelector,
-  formSubmitHandler: function formSubmitHandler(evt) {
-    addCards();
-  }
-});
+const places = '.places';
+const addPlace = document.querySelector('.profile__add-button');
+const placeForm = document.querySelector('.popup__form-place');
 
-const popupProfile = new PopupWithForm({
-  popupSelector: popupProfileSelector,
-  formSubmitHandler: function formSubmitHandler(evt) {
-    evt.preventDefault();
-    userInfo.setUserInfo();
-    popupProfile.close();
-  }
-});
+const template = '.template';
+const inputPlaceName = document.querySelector('.popup__input-field_value_place');
+const inputPlaceUrl = document.querySelector('.popup__input-field_value_placeurl');
 
+const editProfile = document.querySelector('.profile__edit-button');
+
+const formElement = document.querySelector('.popup__form');
+const popupProfileSelector = document.querySelector('.popup_profile');
+const popupPlaceSelector = document.querySelector('.popup_place');
+const popupZoomSelector = document.querySelector('.popup_zoom');
+
+const userName = document.querySelector('.profile__title');
+const job = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__input-field_value_name');
+const jobInput = document.querySelector('.popup__input-field_value_job');
+
+const zoomPlaceImg = document.querySelector('.popup__img');
+const zoomPlaceCaption = document.querySelector('.popup__caption');
+
+
+const popupProfile = new Popup({popupSelector: popupProfileSelector});
+const popupPlace = new Popup({popupSelector: popupPlaceSelector});
 const popupZoom = new PopupWithImage({
   popupSelector: popupZoomSelector,
   imageSelector:  zoomPlaceImg,
@@ -54,16 +43,6 @@ const popupZoom = new PopupWithImage({
 popupProfile.setEventListeners();
 popupPlace.setEventListeners();
 popupZoom.setEventListeners();
-
-const userInfo = new UserInfo({
-  userNameSelector: userName,
-  userBioSelector: job
-});
-
-function openpopupProfile() {
-  userInfo.getUserInfo()
-  popupProfile.open()
-}
 
 
 // renders cards to the page
@@ -84,7 +63,7 @@ const defaultCardList = new Section (
     }
   },
   places
-)
+);
 
 defaultCardList.renderItems();
 
@@ -107,12 +86,29 @@ const addCards = () => {
       );
     const newCardElement = cardItem.generateCard();
 
-    document.querySelector(places).prepend(newCardElement);
+    places.prepend(newCardElement);
     inputPlaceName.value = '';
     inputPlaceUrl.value = '';
     popupPlace.close()
   });
 };
+
+addCards();
+
+
+function openpopupProfile() {
+  nameInput.value = userName.textContent;
+  jobInput.value = job.textContent;
+  popupProfile.open()
+}
+
+//edits profile
+function submitFormHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  popupProfile.close();
+}
 
 //variables used in form validation
 const validationSettings = {
@@ -131,11 +127,13 @@ formElements.forEach((form) => {
 });
 
 
+
+
 editProfile.addEventListener('click', openpopupProfile);
 
 addPlace.addEventListener('click', () => popupPlace.open());
 
-/*formElement.addEventListener('submit', submitFormHandler);*/
+formElement.addEventListener('submit', submitFormHandler);
 
 /*
 // event listeners responsible for different ways to close popups
