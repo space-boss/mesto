33af7c84e2121638d0849dea0048319431b0cfe0
Irina Herzberg <rirarum@yandex.com>
2020-12-16 16:51:24 +0100

    Popup correction

--- src/scripts/components/popup.js
+++ src/scripts/components/popup.js
@@ -1,26 +1,36 @@
 export class Popup {
   constructor({popupSelector}) {
     this._popupSelector = popupSelector;
+    this._popupItem = document.querySelector(this._popupSelector)
+    this._closePopupByClickOnOverlay = this._closePopupByClickOnOverlay.bind(this);
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    this._popupSelector.classList.add('popup_opened');
+    this._popupItem.classList.add('popup_opened');
     document.addEventListener('keydown', this._handleEscClose);
+    this._popupItem.addEventListener('click', this._closePopupByClickOnOverlay)
   }
 
   close() {
-    this._popupSelector.classList.remove('popup_opened');
+    this._popupItem.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
+    this._popupItem.removeEventListener('click', this._closePopupByClickOnOverlay)
   }
 
-   _handleEscClose(event) {
+  _closePopupByClickOnOverlay(event) {
+    if (event.target.classList.contains('popup')) {
+      this._popupItem.classList.remove('popup_opened');
+    }
+  }
+
+  _handleEscClose(event) {
     if (event.key === 'Escape') {
       this.close();
     }
   }
 
   setEventListeners() {
-    this._popupSelector.querySelector('.popup__close').addEventListener('click', this.close.bind(this));
+    this._popupItem.querySelector('.popup__close').addEventListener('click', this.close.bind(this));
   }
 }
