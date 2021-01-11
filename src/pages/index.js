d64385208bf65b2202610e16e22faaa6d8984020
Irina Herzberg <rirarum@yandex.com>
2021-01-11 22:28:15 +0100

    Corrects folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import './styles/index.css';
+import './index.css';
 
-import {Card} from './scripts/components/Card.js';
-import {FormValidator} from './scripts/utils/FormValidator.js';
-import {Section} from './scripts/components/Section.js';
+import {Card} from '../scripts/components/Card.js';
+import {FormValidator} from '../scripts/utils/FormValidator.js';
+import {Section} from '../scripts/components/Section.js';
 
-import {PopupWithImage} from './scripts/components/PopupWithImage.js';
-import {PopupWithForm} from './scripts/components/PopupWithForm.js';
-import {UserInfo} from './scripts/components/UserInfo.js';
+import {PopupWithImage} from '../scripts/components/PopupWithImage.js';
+import {PopupWithForm} from '../scripts/components/PopupWithForm.js';
+import {UserInfo} from '../scripts/components/UserInfo.js';
 
 
 
@@ -28,7 +28,7 @@ import {
   job,
   zoomPlaceImg,
   zoomPlaceCaption
-} from './scripts/utils/Constants.js';
+} from '../scripts/utils/Constants.js';
 
 
 // renders cards to the page
